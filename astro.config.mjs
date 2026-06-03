691426af5e4c51299507ed90b540a77a7e11dc83
munenejoe <joemetha97@gmail.com>
2026-06-04 00:05:30 +0300

    Site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  base: '/munenejoe.github.io/'
-});
 
+export default defineConfig({
+  //  GitHub Pages URL
+  site: 'https://munenejoe.github.io',
+});
